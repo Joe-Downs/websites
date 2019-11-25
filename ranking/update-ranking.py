bcf9a7b779c5f8870357c7ff5a9aa4953b3fc70b
Joe Downs <joedowns502@gmail.com>
2019-11-24 22:06:23 -0500

    update-ranking: beginning of for loop to sort through CSV file and create lists for each column

--- ranking/update-ranking.py
+++ ranking/update-ranking.py
@@ -1,7 +1,18 @@
 import argparse
+import csv
 parser = argparse.ArgumentParser()
-parser.add_argument("filepath", help="Location of CSV file to be updated")
+parser.add_argument("srcFolder", help="Location of CSV file(s) to be updated")
+parser.add_argument("dstFolder", help="Location of updated CSV file(s)")
 parser.add_argument("-v", "--verbosity", action="count", default=0, 
                     help="Increases output verbosity")
 args = parser.parse_args()
-filepath = args.filepath
+src_folder = args.srcFolder
+dst_folder = args.dstFolder
+
+round1 = []
+
+with open(filepath, newline="") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        round1.append(
+        
